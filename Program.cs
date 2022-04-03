0e5402d76d7c712d8196befa3e1d9517c1810fd6
gpdoud <gpdoud@gmail.com>
2022-04-03 08:22:53 -0400

    Final commit
    * added notes

--- Program.cs
+++ Program.cs
@@ -1,44 +1,99 @@
-﻿int FirstUniqueCharacter(string str) {
-
-    List<char> chars = new List<char>();
-    for(var idx = 0; idx < str.Length-1; idx++) {
-        var ch = str[idx];
-        // have already checked this ch
-        if(chars.Contains(ch)) continue;
-        var subStr = str.Substring(idx + 1);
-        // does substr contain another ch?
-        if(!subStr.Contains(ch)) {
+﻿/*
+    Given a string a lowercase characters, return the index of 
+    the first unique character within the string. If no characters
+    are unique, return -1;
+*/
+int IndexOfFirstUniqueCharacter(string str) {
+    // holds characters already found not unique
+    // so the character can be skipped if encountered
+    // more than once
+    List<char> charsNotUnique = new List<char>();
+    // iterate through each character in the string
+    for(var idx = 0; idx < str.Length; idx++) {
+        // get the character to check
+        var targetChar = str[idx];
+        // check if this character has already
+        // been checked. If so, just skip it.
+        if(charsNotUnique.Contains(targetChar)) continue;
+        // if it is the last character and it has not
+        // already been checked, it is unique
+        if(str.Length == idx + 1) return idx;
+        // create an array with the remaining characters
+        var remainingString = str.Substring(idx + 1);
+        // is the character found in the rest of
+        // the characters?
+        if(!remainingString.Contains(targetChar)) {
+            // if not, it is unique! return the index.
             return idx;
         }
-        chars.Add(ch);
+        // the character was found within the rest of the
+        // array. It is NOT unique. Add it to the collection
+        // of characters not unique, and continue
+        charsNotUnique.Add(targetChar);
     }
+    // if we get here, none of the characters are unique.
     return -1;
 }
 
-string[] Anagram(string[] strings) {
-    var sortedStrings = new List<string>();
-    var nonAnagramStrings = new List<string>();
+/*
+    An anagram is a word whose letters can be rearranged to form
+    another word when each characters is used only once.
+
+    Given an array of strings, test each string to see if it
+    is an anagram to another string. If so, discard the second
+    anagram from the collection. Return the collection so that 
+    none of the strings are anagrams to any other string.
+*/
+string[] RemoveAnagrams(string[] strings) {
+    // a collection of the strings where the characters
+    // within the string have been sorted. (i.e. "abcabe"
+    // becomes "aabbcc")
+    var sortedCharacterStrings = new List<string>();
+    // a collection of the strings that have no others
+    // that are anagrams
+    var NoAnagrams = new List<string>();
+    // iterate through all the input strings
     foreach(var str in strings) {
+        // create a string where the characters are 
+        // sorted. (See sortChars method) 
         var strSorted = sortChars(str);
-        if(sortedStrings.Contains(strSorted)) {
+        // check if the sorted string already exists
+        // in the collection of other sorted strings
+        if(sortedCharacterStrings.Contains(strSorted)) {
+            // if found, original string is an anagram
+            // skip it
             continue;
         }
-        sortedStrings.Add(strSorted);
-        nonAnagramStrings.Add(str);
+        // if not found, add the sorted string to the 
+        // collection of other sorted strings that 
+        // are not anagrams
+        sortedCharacterStrings.Add(strSorted);
+        // add the original string to the collection
+        // of non-anagrams
+        NoAnagrams.Add(str);
     }
-    return nonAnagramStrings.ToArray();
+    // return the collection of all the strings that
+    // were found to have no annagrams or where the 
+    // the anagrams were removed.
+    return NoAnagrams.ToArray();
 }
+// Takes a string and sorts the characters within 
+// the string. Two strings are anagrams if the 
+// sorted characters are identical.
+//
+// Give a string input like "dabcbdca", 
+// this returns "aabbccdd";
 string sortChars(string str) {
+    // turn the string into a char[]
     var chArray = str.ToArray<char>();
+    // sort the characters in ascending sequence
     Array.Sort(chArray);
+    // return the sorted characters as a string
     return new string(chArray);
 }
 
-// var strings = Anagram(new string[] { "bac", "acb", "def", "fed", "dfe" });
-// strings.ToList().ForEach(x => System.Console.WriteLine(x));
 
-
-string[] strings = {
+string[] stringsChars = {
 "wolwvolgvkgqtratkyhp",
 "kpwaqkhdnaoqxzyxxrsa",
 "ehaydranjbqajatwddcy",
@@ -49,12 +104,23 @@ string[] strings = {
 "coqmkgfrtzgvmdfynske",
 "osqypnokwvqfimwzbwsb",
 "pqnapvfdlqozsxepwoep",
-"abcabc"
+"abcabcabcabcabcabccx",
+"abcabcabcabcabcabcca"
 };
-foreach(var str in strings) {
-    var index = FirstUniqueCharacter(str);
-    if(index == -1)
-        System.Console.WriteLine($"For {str}, none are unique");
-    else
-        System.Console.WriteLine($"For {str}, index is {index}; char is {str[index]}");
+foreach(var str in stringsChars) {
+    var index = IndexOfFirstUniqueCharacter(str);
+    System.Console.WriteLine(
+        (index == -1) 
+            ? $"For {str}, none are unique" 
+            : $"For {str}, char {str[index]} at [{index}] is unique"
+    );
 }
+
+var WithoutAnagrams = RemoveAnagrams(new string[] { 
+    "bac", "acb", "def", 
+    "fed", "dfe", "edf", 
+    "ghi", "hgi", "ihg"  
+});
+WithoutAnagrams.ToList().ForEach(x => {
+    System.Console.WriteLine(x);
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Programming challenges
+
+See the docs [here](https://gist.github.com/gpdoud/eb4bf5d50da81948530c5f65346a766d)
